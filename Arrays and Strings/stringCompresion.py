6ff703ec4cb188e4d6979c5af3ac0c36cb5255cc
di3g0r <a01741413@tec.mx>
2024-02-19 09:05:21 -0700

    leetCode 443 avance

---
+++ Arrays and Strings/stringCompresion.py
@@ -0,0 +1,54 @@
+"""
+LeetCode 443 (Medium)
+
+"""
+
+def compress(chars: [str]) -> int:
+    char = chars[0]
+    cont = 0
+    master_cont = 0
+    original_len = len(chars)
+    index = 0
+
+    s = ""
+
+    for i in chars:
+        index += 1
+
+        if i  == char:
+            cont+=1
+
+        if i != char or original_len == index:
+            if cont == 1:
+                s += char
+                master_cont +=1
+
+            elif cont >= 10:
+                string_num = str(cont)
+                s += char
+                master_cont +=1
+
+                for j in string_num:
+                    s += j
+                    master_cont +=1
+                    x+=1
+            else:
+                s += char
+                s += str(cont)
+                master_cont += 2
+
+            if index == original_len:
+                print(s)
+                print(master_cont)
+                return master_cont
+            
+            char = chars[index-1]
+            cont = 1
+
+
+    print(s)
+    print(master_cont)
+    return master_cont
+
+chars = ["a","a","b","b","c","c","c"]
+compress(chars)
